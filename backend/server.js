59dbf7e6fc1b3b65cb9a1e48aa2fc1d191b8f823
riyazm786 <riyazm08122004rd@gmail.com>
2026-05-16 20:52:35 +0530

    Fix mongoose connection options

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log('MongoDB connection error:', err));
 
